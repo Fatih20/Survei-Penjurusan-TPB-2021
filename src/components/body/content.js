8dc0b1877e24940cc472c2aac02ee3243c190a66
Fatih20 <fatih@ThinkPad-X230-1b1323db.localdomain>
2021-12-13 17:18:21 +0700

    Jurusan button functional

--- src/components/body/content.js
+++ src/components/body/content.js
@@ -19,10 +19,12 @@ const SelectionContainer = styled.div`
     align-items: center;
     display: flex;
     flex-direction: column;
+    gap: 10px;
 `;
 
 const JurusanContainer = styled.div`
     display: flex;
+    gap: 10px;
 `;
 
 const ButtonActive = css`
@@ -44,14 +46,17 @@ const OptionButton = styled(VanillaButton)`
     padding: 7px;
 `;
 
+const JurusanButton = styled(OptionButton)`
+`;
+
 
 
 export default function Content ({facultyShown}){
     const[jurusanClicked, setJurusanClicked] = useState("None");
 
     useEffect(() => {
-        console.log(facultiesData);
-    });
+        setJurusanClicked("None");
+    }, [facultyShown])
 
     function jurusanClick (buttonClicked) {
         if(buttonClicked === jurusanClicked) {
@@ -61,13 +66,17 @@ export default function Content ({facultyShown}){
         }
     }
 
+    function jurusanButtonCreator(namaJurusan){
+        return <JurusanButton key={namaJurusan} onClick={() => jurusanClick(namaJurusan)} isActive={jurusanClicked == namaJurusan ? true : false}>{namaJurusan}</JurusanButton>
+    }
+
     if (facultyShown != "None"){
         return (
             <Main>
                 <SelectionContainer>
                     <OptionButton onClick={() => jurusanClick("Overview")} isActive={jurusanClicked == "Overview" ? true : false}>Overview</OptionButton>
                     <JurusanContainer>
-                        {/* {facultiesData["STEI"].jurusan.map()} */}
+                        {facultiesData["STEI"]["dataJurusan"].map(jurusanButtonCreator)}
                     </JurusanContainer>
                 </SelectionContainer>
                 <h2>{`Insert ${facultyShown} data here`}</h2>
