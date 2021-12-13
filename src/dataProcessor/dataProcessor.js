8a8b1a5cd4a75773bc6148f73a435406059a3d2a
Fatih20 <fatih@ThinkPad-X230-1b1323db.localdomain>
2021-12-13 19:48:37 +0700

    Creation of function to spit out the data Recharts want

--- src/dataProcessor/dataProcessor.js
+++ src/dataProcessor/dataProcessor.js
@@ -1,11 +1,23 @@
-export function peminat(rawData, ranking){
-    let result = [];
-    const examinedData = rawData[ranking];
-    Object.keys(examinedData).map((jurusan) => {
-        result.push({
-            namaJurusan : jurusan,
-            jumlah : examinedData[jurusan],
-        })
+export function dataPeminatProcessed(data,ranking){
+    const rankedData = data[ranking.toString()]
+    const result = Object.keys(rankedData).map((jurusan) => {
+        return {
+            "namaJurusan" : jurusan,
+            "jumlahPeminat" : rankedData[jurusan],
+        }
     })
+
+    return result;
+}
+
+export function dataIndeksPeminatProcessed(data, ranking) {
+    const rankedData = data[ranking.toString()]
+    const result = Object.keys(rankedData).map((jurusan) => {
+        return {
+            "namaJurusan" : jurusan,
+            "indeksPeminat" : rankedData[jurusan],
+        }
+    })
+
     return result;
 }
